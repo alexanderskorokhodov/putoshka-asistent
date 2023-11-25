7be4a7ca26aa72e80d23221b654e959c7f0ea35a
macbook <playegrovoy@gmail.com>
2023-11-25 13:24:00 +0300

    stable

--- shared/src/commonMain/kotlin/com/example/contestapp/resource/Constants.kt
+++ shared/src/commonMain/kotlin/com/example/contestapp/resource/Constants.kt
@@ -3,7 +3,7 @@ package com.example.contestapp.resource
 object Constants{
 
     const val LECTURE_TITLE = "Лекции"
-    const val API_URL = "https://4ba4-89-109-249-13.ngrok-free.app"
+    const val API_URL = "https://3dff-34-28-118-121.ngrok.io"
     const val ADD_LECTURE_TITLE = "Добавить Лекцию"
     const val FIRST_FILTER_MODE = 0
     const val SECOND_FILTER_MODE = 1
